b6e39ed2cf72e275b03cf3b227d88421712ef8cc
MariaGargoles <mariagargoles@live.com>
2024-12-09 20:42:39 +0100

    added swipper, doesnt works will try do on typescript

--- src/components/ProyectsComponent/SwiperProyects.jsx
+++ src/components/ProyectsComponent/SwiperProyects.jsx
@@ -1,12 +1,27 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css'; 
+import { Pagination, Navigation } from 'swiper/modules'; 
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import React from 'react';
 
-const SwiperComponent = () => {
+export const SwiperComponent = () => {
   return (
-    <Swiper spaceBetween={50} slidesPerView={1}>
+    <Swiper
+      pagination={true} 
+      navigation={true} 
+      modules={[Pagination, Navigation]} 
+      className="mySwiper"
+    >
       <SwiperSlide>Slide 1</SwiperSlide>
       <SwiperSlide>Slide 2</SwiperSlide>
       <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      <SwiperSlide>Slide 5</SwiperSlide>
+      <SwiperSlide>Slide 6</SwiperSlide>
+      <SwiperSlide>Slide 7</SwiperSlide>
+      <SwiperSlide>Slide 8</SwiperSlide>
+      <SwiperSlide>Slide 9</SwiperSlide>
     </Swiper>
   );
 };
