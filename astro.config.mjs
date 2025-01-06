abb7b5c3329f69f101502725810152300eff47b2
MariaGargoles <mariagargoles@live.com>
2025-01-06 18:59:11 +0100

    test 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,20 @@
-// @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-// https://astro.build/config
+import viteStaticCopy from "vite-plugin-static-copy";
+
 export default defineConfig({
   integrations: [tailwind(), react()],
+  vite: {
+    plugins: [
+      viteStaticCopy({
+        targets: [
+          {
+            src: "public/assets/*",
+            dest: "assets",
+          },
+        ],
+      }),
+    ],
+  },
 });
