a0be278b399f4b4881b45ff458358fec3086a557
MariaGargoles <mariagargoles@live.com>
2025-08-11 16:44:58 +0200

    Me comi una T, haha

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://mariagargoles.github.io",
-  base: "/Portfolio-Astro/",
+  base: "/Porfolio-Astro/",
   integrations: [tailwind(), react()],
   vite: {
     server: {
